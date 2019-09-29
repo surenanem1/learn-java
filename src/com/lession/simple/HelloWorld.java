396b76d14b29307dc01625c83939046d5d5d7ac9
suren <suren@surenanem.com>
2019-09-29 15:41:22 -0500

    Initial code commit

---
+++ src/com/lession/simple/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.lession.simple;
+
+/**
+ * @author suren
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String ...args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world.");
+
+	}
+
+}
